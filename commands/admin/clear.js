55d680d6d8e98c40f1353b7af788746bd91f44f4
yushi-dev <ja.agripo2003@gmail.com>
2022-01-07 23:50:10 +0100

    minor changes in code

--- commands/admin/clear.js
+++ commands/admin/clear.js
@@ -8,17 +8,11 @@ module.exports = {
             description: "specify an amount of messages to be removed",
             type: 10,
             required: true
-        },
-        {
-            name: "member",
-            description: "delete messages only from the specified member",
-            type: 6,
         }
     ],
 
     async execute(client, interaction) {
         const amount = interaction.options.getNumber("number");
-        const member = interaction.options.getMember("member");
 
         if (amount < 1) {
             await interaction.reply(
@@ -42,17 +36,11 @@ module.exports = {
             return;
         }
 
-        let messages = interaction.channel.messages.fetch();
-
-        if (member) {
-            messages = (await messages).filter((message) => message.author.id === member.id);
-        }
-
-        await interaction.channel.bulkDelete(messages, true);
+        await interaction.channel.bulkDelete(amount, true);
 
         await interaction.reply(
             {
-                content: `i have deleted \`${amount}\` ${member ? `messages of ${member.tag}` : "messages"}`,
+                content: `i have deleted \`${amount}\` messages`,
                 ephemeral: true
             }
         );
