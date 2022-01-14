9a8eac50aef44741ef75c1042e086349227a5d14
yushi-dev <ja.agripo2003@gmail.com>
2022-01-14 03:25:20 +0100

    added leaderboard mongoose

--- models/memberSchema.js
+++ models/memberSchema.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema(
     {
-        userId: { type: String, required: true, unique: true },
-        guildId: { type: String, required: true },
+        guildId: { type: String, required: true, unique: true },
+        userId: { type: String, required: true },
         userTag: { type: String, required: true },
         messages: { type: Number, default: 0 }
     }
