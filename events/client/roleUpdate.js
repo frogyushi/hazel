075228afe319c757ffd0b0b813bf0e55c9a9072f
yushi-dev <ja.agripo2003@gmail.com>
2022-01-14 13:41:45 +0100

    minor fixes for global command permissions

--- events/client/roleUpdate.js
+++ events/client/roleUpdate.js
@@ -1,9 +1,35 @@
 module.exports = {
     name: "roleUpdate",
 
-    async execute(client) {
+    async execute(client, oldRole, newRole) {
+        const commands = await client.application.commands.fetch();
+
+        const { roles, id } = oldRole.guild.cache.values() || newRole.guild.cache.values();
+
+        const fullPermissions = [];
+        for (const command of commands.values()) {
+            const { permissions: perms } = await client.commands.get(command.name);
+
+            if (!perms) continue;
+
+            let permissions = [];
+            for (const perm of perms) {
+                const role = roles.cache.find((role) => role.name === perm)?.id || roles.cache.get(perm);
+                if (!role) continue;
+                permissions.push(
+                    {
+                        id: role,
+                        type: 1,
+                        permission: true
+                    }
+                );
+
+                fullPermissions.push({ id: command.id, permissions });
+            }
+        }
+
         await client.REST.put(
-            Routes.guildApplicationCommandsPermissions(this.id, id),
+            Routes.guildApplicationCommandsPermissions(client.id, id),
             {
                 body: fullPermissions
             },
