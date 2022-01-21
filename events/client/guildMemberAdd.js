c2ab04608b0fc05fcc595eb5f19f648cdd9f5236
yushi-dev <ja.agripo2003@gmail.com>
2022-01-21 10:54:52 +0100

    minor fixes/changes in code

--- events/client/guildMemberAdd.js
+++ events/client/guildMemberAdd.js
@@ -6,54 +6,33 @@ module.exports = {
 	name: "guildMemberAdd",
 
 	async execute(client, member) {
-		const welcomeMessage = await welcomeMessageSchema.findOne({
-			guildId: member.guild.id,
-		});
-
-		const channel = member.guild.channels.cache.get(welcomeMessage.channelId);
-
+		const welcome = await welcomeMessageSchema.findOne({ guildId: member.guild.id });
+		const roles = await welcomeRoleSchema.find({ guildId: member.guild.id });
+		const channel = member.guild.channels.cache.get(welcome.channelId);
 		const embed = new MessageEmbed();
 
-		const roles = await welcomeRoleSchema.find({
-			guildId: member.guild.id,
-		});
+		if (!welcome?.isEnabled || !channel) return;
 
-		if (!welcomeMessage?.isEnabled || !channel) return;
+		if (welcome.color) embed.setColor(welcome.color);
+		if (welcome.image) embed.setImage(welcome.image);
+		if (welcome.footer) embed.setFooter({ text: welcome.footer });
+		if (welcome.timestamp) embed.setTimestamp();
 
-		if (welcomeMessage.color) {
-			embed.setColor(welcomeMessage.color);
+		if (welcome.title) {
+			const title = welcome.title.replace(/{member}/gi, member.user.tag).replace(/{guild}/gi, member.guild.name);
+
+			embed.setTitle(title);
 		}
 
-		if (welcomeMessage.title) {
-			const title = welcomeMessage.title
-				.replace(/{member}/gi, member.user.tag)
-				.replace(/{guild}/gi, member.guild.name);
-
-			embed.setTitle({ name: title });
-		}
-
-		if (welcomeMessage.description) {
-			const description = welcomeMessage.description
+		if (welcome.description) {
+			const description = welcome.description
 				.replace(/{member}/gi, `<@${member.user.id}>`)
 				.replace(/{guild}/gi, member.guild.name);
 
 			embed.setDescription(description);
 		}
 
-		if (welcomeMessage.image) {
-			embed.setImage(welcomeMessage.image);
-		}
-
-		if (welcomeMessage.footer) {
-			embed.setFooter({ text: welcomeMessage.footer });
-		}
-
-		if (welcomeMessage.timestamp) {
-			embed.setTimestamp();
-		}
-
 		channel.send({ embeds: [embed] });
-
 		member.roles.add(roles.map(({ roleId }) => roleId));
 	},
 };
