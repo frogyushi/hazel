9a6ab0bd9bf7c06e0022d8b0bf78c7ccdc9d13a6
yushi-dev <ja.agripo2003@gmail.com>
2022-04-29 17:44:27 +0200

    make it better

--- events/client/ready.js
+++ events/client/ready.js
@@ -3,10 +3,6 @@ module.exports = {
 
 	async execute(client) {
 		client.user.setActivity("Valorant", { type: "COMPETING" });
-
-		client.setSlashPerms();
-		client.registerSlashCommands();
-
 		client.logger.success("Client is ready");
 	},
 };
