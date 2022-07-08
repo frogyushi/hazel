2eed8a3405baf0429e0d31656933b6ad97830e75
frogyushi <ja.agripo2003@gmail.com>
2022-07-09 00:43:45 +0200

    nieuwe versie

--- commands/admin/embed.js
+++ commands/admin/embed.js
@@ -29,7 +29,7 @@ module.exports = {
         const embed = new MessageEmbed()
             .setColor("#6C78AD")
             .setTitle(title)
-            .setDescription(description.replace(/\n/, "\n"));
+            .setDescription(description.replace(/\n/g, "\n"));
 
         interaction.channel.send({ embeds: [embed] });
     },
