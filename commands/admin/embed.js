e6a89de939bfd56dc953a1c122955962ec871842
frogyushi <ja.agripo2003@gmail.com>
2022-07-09 00:21:39 +0200

    nieuwe versie

---
+++ commands/admin/embed.js
@@ -0,0 +1,36 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "embed",
+    description: "Create an embed",
+    ownerOnly: true,
+    options: [
+        {
+            name: "title",
+            description: "Set a title",
+            type: 3,
+            required: true,
+        },
+        {
+            name: "description",
+            description: "Set description",
+            type: 3,
+            required: true,
+        },
+    ],
+
+    async execute(client, interaction) {
+        const title = interaction.options.getString("title");
+        const description = interaction.options.getString("description");
+
+        await interaction.deferReply();
+        await interaction.deleteReply();
+
+        const embed = new MessageEmbed()
+            .setColor("#6C78AD")
+            .setTitle(title)
+            .setDescription(description);
+
+        interaction.channel.send({ embeds: [embed] });
+    },
+};
